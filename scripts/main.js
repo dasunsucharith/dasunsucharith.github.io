17663b91d3578550debdcf175701f1a80b17a155
Dasun Sucharith <sucharith.dasun@gmail.com>
2015-12-22 22:05:26 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-/*var myImage = document.querySelector('img');
+var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
@@ -7,26 +7,7 @@ myImage.onclick = function() {
     } else {
       myImage.setAttribute ('src','images/dasun2.jpg');
     }
-}*/
-
-(function () {
-    var rotator = document.getElementById('rotator'); // change to match image ID
-    var imagesrc = 'images/dasun.jpg';
-    var imageDir = 'images/';
-    var delayInSeconds = 5;
-    // set number of seconds delay
-    // list image names
-    var images = ['dasun1.jpg', 'dasun2.jpg'];
-    var num = 0;
-    var changeImage = function () {
-        var len = images.length;
-        rotator.src = imageDir + images[num++];
-        if (num == len) {
-            num = 0;
-        }
-    };
-    setInterval(changeImage, delayInSeconds * 1000);
-})();
+}
 
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
