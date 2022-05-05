42368975d175c520a4454fa8e5abf569575aa9e4
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-05-05 08:27:31 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -20,12 +20,22 @@ totalSelection = allSelection.length
 for (let i = 0; i < totalNavlist; i++){
     const a = navList[i].querySelector("a")
     a.addEventListener("click", function() {
+        for (let i = 0; i < totalSelection; i++){
+            allSelection[i].classList.remove("back-section")
+        }
         for (let j = 0; j < totalNavlist; j++){
+            if(navList[j].querySelector("a").classList.contains("active")){
+                allSelection[j].classList.add("back-section")
+            }
             navList[j].querySelector("a").classList.remove("active")
         }
 
         this.classList.add("active")
         showSelection(this)
+
+        if (window.innerWidth < 1200){
+            asideSectionTogglerBtn()
+        }
     })
 }
 
@@ -36,3 +46,24 @@ function showSelection(element){
     const target = element.getAttribute("href").split("#")[1]
     document.querySelector("#" + target).classList.add("active")
 }
+
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+    aside = document.querySelector(".aside"),
+    mainContent = document.querySelector(".main-content")
+    navTogglerBtn.addEventListener("click", () => {
+        asideSectionTogglerBtn()
+        mainContentTogglerBtn()
+    })
+    function asideSectionTogglerBtn() {
+        aside.classList.toggle("open")
+        navTogglerBtn.classList.toggle("open")
+        for (let i = 0; i < totalSelection; i++){
+            allSelection[i].classList.toggle("open")
+        }
+    }
+    /* function mainContentTogglerBtn() {
+        if(aside.classList.contains("open"))
+        {
+            aside.classList.remove("open")
+        }
+    } */
