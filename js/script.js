15d0df71cb8635f90669b20783ee9bbc28906609
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-05-13 12:00:59 +0530

    animated skill bar

--- js/script.js
+++ js/script.js
@@ -118,3 +118,18 @@ function sendEmail() {
       message => alert(message)
     )
 }
+
+$('.progress-in').each(function(){
+    var $this = $(this)
+    var per = $this.attr('per')
+    $this.css("width", per + '%')
+    $({animatedValue: 0}).animate({animatedValue: per},{
+        duration: 1000,
+        step: function(){
+            $this.attr('per', Math.floor(this.animatedValue) + '%')
+        },
+        complete: function(){
+            $this.attr('per', Math.floor(this.animatedValue) + '%')
+        }
+    })
+})
