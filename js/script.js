2cae7c97c8687d336a4f316f53cd2558f556bd60
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-05-22 11:42:35 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var typed = new Typed(".typing", {
-    strings: ["","Web Designer", "Web Developer", "Graphic Designer", "YouTuber"],
+    strings: ["","Web Designer", "Web Developer", "Software Developer", "Android Developer", "Graphic Designer", "YouTuber"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
 })
 
 var typed = new Typed(".typing-about", {
-    strings: ["","Web Designer", "Web Developer", "Graphic Designer", "YouTuber"],
+    strings: ["","Web Designer", "Web Developer", "Software Developer", "Android Developer", "Graphic Designer", "YouTuber"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
